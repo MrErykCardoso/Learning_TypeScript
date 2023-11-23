09b6954d6c5c7ff659f6d42fa8ea3bf69181c2f7
MrErykCardoso <mr.erykcardoso@gmail.com>
2023-11-23 15:50:06 -0300

    modified:   dist/js/index.js 	modified:   src/index.ts 	modified:   tsconfig.json

--- dist/js/index.js
+++ dist/js/index.js
@@ -10,7 +10,30 @@ console.log(y);
 let z = 70; //infere automaticamente como number;
 let c = 70; //Annotation é o padrão, mais cumprido;
 // tipos básicos
-let firstname = 'marcelo';
+let firstName = 'marcelo';
 let age = 23;
 let isAdmin = true;
-console.log(firstname, age, isAdmin);
+console.log(firstName, age, isAdmin);
+console.log(typeof firstName);
+// Arrays
+const numbers = [1, 2, 3];
+const clientNames = ['joao', 'maria', 'jose'];
+console.log(numbers);
+console.log(clientNames);
+console.log(numbers.length);
+console.log(firstName.toUpperCase());
+console.log(clientNames[0].toUpperCase());
+numbers.push(4, 5);
+//tuplas -> Como um array, mas predefinido com tipos de variáveis diferentes;
+let myTuple; //Tipos de variáveis
+myTuple = [1, 'nome', [1, 2, 3]];
+console.log(myTuple);
+//Object Literals -> {propriedade: valor} //porde ser usado para criar um objeto com várias características, porém, travado pela propriedade definida na primeira chave.
+const user = {
+    name: "marcelo",
+    age: 23,
+    email: 'takakaranomuro30@gmail.com',
+};
+//  user.job = 'programador'; Não pode ser adicionado, pois excede as propriedades, mesmo que não fosse variavel "const";
+console.log(user);
+//
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,39 @@ let z = 70; //infere automaticamente como number;
 let c: number = 70; //Annotation é o padrão, mais cumprido;
 
 // tipos básicos
-let firstname: string = 'marcelo';
+let firstName: string = 'marcelo';
 let age: number = 23;
 let isAdmin: boolean = true;
 
-console.log(firstname, age, isAdmin);
+console.log(firstName, age, isAdmin);
+console.log(typeof firstName);
+
+// Arrays
+const numbers: number[] = [1, 2, 3];
+const clientNames: string[] = ['joao', 'maria', 'jose'];
+console.log(numbers);
+console.log(clientNames);
+console.log(numbers.length);
+console.log(firstName.toUpperCase());
+console.log(clientNames[0].toUpperCase());
+numbers.push(4, 5);
+
+//tuplas -> Como um array, mas predefinido com tipos de variáveis diferentes;
+let myTuple: [number, string, number[]]; //Tipos de variáveis
+
+myTuple = [1, 'nome', [1, 2, 3]];
+
+console.log(myTuple);
+
+//Object Literals -> {propriedade: valor} //porde ser usado para criar um objeto com várias características, porém, travado pela propriedade definida na primeira chave.
+const user: { name: string; age: number; email: string } = {
+    name: "marcelo",
+    age: 23,
+    email: 'takakaranomuro30@gmail.com',
+}
+
+//  user.job = 'programador'; Não pode ser adicionado, pois excede as propriedades, mesmo que não fosse variavel "const";
+
+console.log(user);
+
+//
