6f9f8d34493f357ae588b08dbaa137fb3389c018
MrErykCardoso <mr.erykcardoso@gmail.com>
2023-11-25 07:46:37 -0300

    modified:   dist/index.html 	modified:   dist/js/index.js 	modified:   src/index.ts 	modified:   tsconfig.json

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,84 +1,52 @@
 "use strict";
-//Variáveis
-let x = 50;
-let y = 'maria';
-x = 60; // reescrita pra let, imutavel pra const;
-// x = 'string'; -> erro, não permite mudança de tipo de variavel de forma dinâmica;
-console.log(x);
-console.log(y);
-//--------------------------------------------------------------------------------------------
-//inferencia x annotation
-let z = 70; //infere automaticamente como number;
-let c = 70; //Annotation é o padrão, mais cumprido;
-//--------------------------------------------------------------------------------------------
-// tipos básicos
-let firstName = 'marcelo';
-let age = 23;
-let isAdmin = true;
-console.log(firstName, age, isAdmin);
-console.log(typeof firstName);
-//--------------------------------------------------------------------------------------------
-// Arrays
-const numbers = [1, 2, 3];
-const clientNames = ['joao', 'maria', 'jose'];
-console.log(numbers);
-console.log(clientNames);
-console.log(numbers.length);
-console.log(firstName.toUpperCase());
-console.log(clientNames[0].toUpperCase());
-numbers.push(4, 5);
-//--------------------------------------------------------------------------------------------
-//tuplas -> Como um array, mas predefinido com tipos de variáveis diferentes;
-let myTuple; //Tipos de variáveis
-myTuple = [1, 'nome', [1, 2, 3]];
-console.log(myTuple);
-//--------------------------------------------------------------------------------------------
-//Object Literals -> {propriedade: valor} //porde ser usado para criar um objeto com várias características, porém, travado pela propriedade definida na primeira chave.
-const user = {
-    name: "marcelo",
-    age: 23,
-    email: 'takakaranomuro30@gmail.com',
-};
-//  user.job = 'programador'; Não pode ser adicionado, pois excede as propriedades, mesmo que não fosse variavel "const";
-console.log(user);
-//Object but Unior type -> uma forma de expandir a forma de object literals
-let id; //aqui se declara a variável com mais de uma tipologia
-id = 'almanaque';
-id = 10; //ela pode levar valores de mais de um tipo
-let nome = 'Marcelo';
-let idade = 23;
-let isAdm = true; //observe como o tipo criado carrega todos os tipos declarados sem problema;
-// let numerosFavoritos: myIdType = [1,2,3]; exceto aos que excedem os tipos especificados na criação
-//--------------------------------------------------------------------------------------------
-// enum -> pode ser usado para encurtar a utilização e declaração de variáveis que se repetem
-var size;
-(function (size) {
-    size["p"] = "pequeno";
-    size["m"] = "m\u00E9dio";
-    size["g"] = "grande";
-})(size || (size = {}));
-const camisa = {
-    nome: "camisa kkk",
-    size: size.g,
-};
-console.log(camisa); //a saída da variável "size" de camisa fica com o valo de g em "enum size";
-//--------------------------------------------------------------------------------------------
-// Literal types -> criar uma variável instantânea que acompanha a variável. Apenas pode ser auterada para a variável declarada;
-let literal; //pode ser usado em conjunto com o "null" para variar entre os dois tipos
-// literal = "outro tipo ou valor qualquer" -> não funciona;
-literal = null;
-literal = "autenticado"; //apenas
-//--------------------------------------------------------------------------------------------
-//funçoes
-function soma(a, b) {
-    return a + b; //lembre sempre de específicar os timos das variáveis recebias.
+class User {
+    name;
+    role;
+    isAdmin;
+    constructor(name, role, isAdmin) {
+        this.name = name;
+        this.role = role;
+        this.isAdmin = isAdmin;
+    }
+    showUserName() {
+        console.log(`Nome do Usuário: ${this.name}`);
+    }
+    showUserRole(canShow) {
+        if (canShow) {
+            console.log(`O nível de usuário é ${this.role}`);
+        }
+        else {
+            console.log(`Acesso negado!`);
+        }
+    }
 }
-console.log(soma(5, 5));
-function sayHelloTo(name) {
-    return `Hello to ${name}`; //strings com váriáveis pelo meio usam ``
+const joaozinho = new User('Joaozinho', "Admin", true);
+console.log(joaozinho);
+joaozinho.showUserName();
+joaozinho.showUserRole(false);
+class vehicle {
+    brand;
+    whell;
+    maxSpeed;
+    constructor(brand, whell, maxSpeed) {
+        this.brand = brand;
+        this.whell = whell;
+        this.maxSpeed = maxSpeed;
+    }
+    showBrand() {
+        console.log(`A marca do vehículo é ${this.brand}.`);
+    }
 }
-console.log(sayHelloTo('Marcelo'));
-function logger(msg) {
-    console.log(msg); //apesar de o "console.log" retornar para o terminal ele não retorna nenhum valor ao código, por isso a função é vazia
+const fiatUnoDeFirma = new vehicle('Fiat', 16, 'luz');
+console.log(fiatUnoDeFirma);
+fiatUnoDeFirma.showBrand();
+class superCar extends vehicle {
+    engine;
+    constructor(brand, wheel, maxSpeed, engine) {
+        super(brand, wheel, maxSpeed);
+        this.engine = engine;
+    }
 }
-logger('não retorno nada');
+const Veiron = new superCar('Bugati', 20, '300km/h', 'v8');
+console.log(Veiron);
+Veiron.showBrand();
--- src/index.ts
+++ src/index.ts
@@ -1,128 +1,267 @@
-//Variáveis
-let x: number = 50;
-let y: string = 'maria';
+// //Variáveis
+// let x: number = 50;
+// let y: string = 'maria';
 
-x = 60; // reescrita pra let, imutavel pra const;
-// x = 'string'; -> erro, não permite mudança de tipo de variavel de forma dinâmica;
+// x = 60; // reescrita pra let, imutavel pra const;
+// // x = 'string'; -> erro, não permite mudança de tipo de variavel de forma dinâmica;
 
-console.log(x);
-console.log(y);
+// console.log(x);
+// console.log(y);
 
-//--------------------------------------------------------------------------------------------
-
-
-//inferencia x annotation
-let z = 70; //infere automaticamente como number;
-let c: number = 70; //Annotation é o padrão, mais cumprido;
-
-//--------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------
 
 
-// tipos básicos
-let firstName: string = 'marcelo';
-let age: number = 23;
-let isAdmin: boolean = true;
+// //inferencia x annotation
+// let z = 70; //infere automaticamente como number;
+// let c: number = 70; //Annotation é o padrão, mais cumprido;
 
-console.log(firstName, age, isAdmin);
-console.log(typeof firstName);
-
-//--------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------
 
 
-// Arrays
-const numbers: number[] = [1, 2, 3];
-const clientNames: string[] = ['joao', 'maria', 'jose'];
-console.log(numbers);
-console.log(clientNames);
-console.log(numbers.length);
-console.log(firstName.toUpperCase());
-console.log(clientNames[0].toUpperCase());
-numbers.push(4, 5);
+// // tipos básicos
+// let firstName: string = 'marcelo';
+// let age: number = 23;
+// let isAdmin: boolean = true;
 
-//--------------------------------------------------------------------------------------------
+// console.log(firstName, age, isAdmin);
+// console.log(typeof firstName);
+
+// //--------------------------------------------------------------------------------------------
 
 
-//tuplas -> Como um array, mas predefinido com tipos de variáveis diferentes;
-let myTuple: [number, string, number[]]; //Tipos de variáveis
+// // Arrays
+// const numbers: number[] = [1, 2, 3];
+// const clientNames: string[] = ['joao', 'maria', 'jose'];
+// console.log(numbers);
+// console.log(clientNames);
+// console.log(numbers.length);
+// console.log(firstName.toUpperCase());
+// console.log(clientNames[0].toUpperCase());
+// numbers.push(4, 5);
 
-myTuple = [1, 'nome', [1, 2, 3]];
-
-console.log(myTuple);
-
-//--------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------
 
 
-//Object Literals -> {propriedade: valor} //porde ser usado para criar um objeto com várias características, porém, travado pela propriedade definida na primeira chave.
-const user: { name: string; age: number; email: string } = {
-    name: "marcelo",
-    age: 23,
-    email: 'takakaranomuro30@gmail.com',
+// //tuplas -> Como um array, mas predefinido com tipos de variáveis diferentes;
+// let myTuple: [number, string, number[]]; //Tipos de variáveis
+
+// myTuple = [1, 'nome', [1, 2, 3]];
+
+// console.log(myTuple);
+
+// //--------------------------------------------------------------------------------------------
+
+
+// //Object Literals -> {propriedade: valor} //porde ser usado para criar um objeto com várias características, porém, travado pela propriedade definida na primeira chave.
+// const user: { name: string; age: number; email: string } = {
+//     name: "marcelo",
+//     age: 23,
+//     email: 'takakaranomuro30@gmail.com',
+// }
+
+// //  user.job = 'programador'; Não pode ser adicionado, pois excede as propriedades, mesmo que não fosse variavel "const";
+
+// console.log(user);
+
+// //Object but Unior type -> uma forma de expandir a forma de object literals
+
+// let id: string | number //aqui se declara a variável com mais de uma tipologia
+
+// id = 'almanaque';
+// id = 10; //ela pode levar valores de mais de um tipo
+// // id = true; exceto se excederem os tipos especificados na declaração
+
+// //--------------------------------------------------------------------------------------------
+
+
+// //Type alias -> Criar uma variável que pode carregar valores de vários tipos desde que especificados na declaração;
+
+// type myIdType = number | string | boolean;
+
+// let nome: myIdType = 'Marcelo';
+// let idade: myIdType = 23;
+// let isAdm: myIdType = true; //observe como o tipo criado carrega todos os tipos declarados sem problema;
+// // let numerosFavoritos: myIdType = [1,2,3]; exceto aos que excedem os tipos especificados na criação
+
+// //--------------------------------------------------------------------------------------------
+
+
+// // enum -> pode ser usado para encurtar a utilização e declaração de variáveis que se repetem
+
+// enum size { //criação da enumeração
+//     p = 'pequeno',
+//     m = 'médio',
+//     g = 'grande',
+// }
+// const camisa = { //utilização da enumeração em uma objetos
+//     nome: "camisa kkk",
+//     size: size.g,
+// }
+// console.log(camisa); //a saída da variável "size" de camisa fica com o valo de g em "enum size";
+
+// //--------------------------------------------------------------------------------------------
+
+// // Literal types -> criar uma variável instantânea que acompanha a variável. Apenas pode ser auterada para a variável declarada;
+
+// let literal: "autenticado" | null; //pode ser usado em conjunto com o "null" para variar entre os dois tipos
+// // literal = "outro tipo ou valor qualquer" -> não funciona;
+// literal = null;
+// literal = "autenticado"; //apenas
+
+// //------------------------------------------------------------------------------------------
+
+// //funçoes
+
+// function soma(a: number, b: number): number { //explicite o que a função irá retornar
+//     return a + b; //lembre sempre de específicar os timos das variáveis recebias.
+// }
+// console.log(soma(5, 5));
+
+// function sayHelloTo(name: string): string {
+//     return `Hello to ${name}`; //strings com váriáveis pelo meio usam ``
+// }
+// console.log(sayHelloTo('Marcelo'));
+
+// function logger(msg: string): void { //usando o tipo "void" para especificar o tipo de função que não gera retorno
+//     console.log(msg); //apesar de o "console.log" retornar para o terminal ele não retorna nenhum valor ao código, por isso a função é vazia
+// }
+// logger('não retorno nada');
+
+// function greeting(name: string, greet?: string): void { //usando "?" no argumento podemos informamos que o dado ao seu lado é opcional que se não recebe nenhum dado é definido como "undefined".
+//     if (greet) {
+//         console.log(`Olá ${greet} ${name}.`);
+//     }
+//     else {
+//         console.log(`Olá ${name}.`);
+//     }
+// }
+// greeting('Eryk');
+// greeting('Eryk', 'Mr.');
+
+// //------------------------------------------------------------------------------------------
+
+// //Insterfaces
+// interface mathNumbers {
+//     n1: number;
+//     n2: number;
+//     n3: number;
+// }
+
+// function sumMult(numeros: mathNumbers) {
+//     return (numeros.n1 + numeros.n2) * numeros.n3;
+// }
+// console.log(sumMult({ n1: 2, n2: 2, n3: 3 }));
+
+// function multDiv(numeros: mathNumbers) {
+//     return (numeros.n1 * numeros.n2) / numeros.n3;
+// }
+// console.log(multDiv({ n1: 3, n2: 3, n3: 3 }));
+
+// //------------------------------------------------------------------------------------------
+
+// //narrowing -> checagem de tipos
+
+// function doSomeThing(variavel: number | boolean) {
+//     if (typeof variavel == "number" || typeof variavel == "boolean") {
+//         console.log(`A variável é ${variavel}`);
+//     }
+//     else {
+//         console.log('variável não informada');
+//     }
+// }
+// doSomeThing(true);
+
+// //------------------------------------------------------------------------------------------
+
+// //generics -> chama uma array generica definida por "<qualquer>" que quando aplicada na função permite que ela receba qualquer tipode de array
+// function showArrayItems<T>(arr: T[]) {
+//     arr.forEach((item) => {
+//         console.log(`Item: ${item}`);
+//     });
+
+//     const a1 = [1, 2, 3];
+//     const a2 = ["jesus", "maria", "jose"];
+
+//     showArrayItems(a1);
+//     showArrayItems(a2);
+// }
+
+//------------------------------------------------------------------------------------------
+
+//classes
+
+class User {
+    name;
+    role;
+    isAdmin;
+
+    constructor(name: string, role: string, isAdmin: boolean) {
+        this.name = name;
+        this.role = role;
+        this.isAdmin = isAdmin;
+    }
+
+    showUserName() {
+        console.log(`Nome do Usuário: ${this.name}`);
+    }
+
+    showUserRole(canShow: boolean) {
+        if (canShow) {
+            console.log(`O nível de usuário é ${this.role}`);
+        }
+        else {
+            console.log(`Acesso negado!`);
+        }
+    }
 }
 
-//  user.job = 'programador'; Não pode ser adicionado, pois excede as propriedades, mesmo que não fosse variavel "const";
+const joaozinho = new User('Joaozinho', "Admin", true);
+console.log(joaozinho);
+joaozinho.showUserName();
+joaozinho.showUserRole(false);
 
-console.log(user);
+//Insterfaces em classes
 
-//Object but Unior type -> uma forma de expandir a forma de object literals
-
-let id: string | number //aqui se declara a variável com mais de uma tipologia
-
-id = 'almanaque';
-id = 10; //ela pode levar valores de mais de um tipo
-// id = true; exceto se excederem os tipos especificados na declaração
-
-//--------------------------------------------------------------------------------------------
-
-
-//Type alias -> Criar uma variável que pode carregar valores de vários tipos desde que especificados na declaração;
-
-type myIdType = number | string | boolean;
-
-let nome: myIdType = 'Marcelo';
-let idade: myIdType = 23;
-let isAdm: myIdType = true; //observe como o tipo criado carrega todos os tipos declarados sem problema;
-// let numerosFavoritos: myIdType = [1,2,3]; exceto aos que excedem os tipos especificados na criação
-
-//--------------------------------------------------------------------------------------------
-
-
-// enum -> pode ser usado para encurtar a utilização e declaração de variáveis que se repetem
-
-enum size { //criação da enumeração
-    p = 'pequeno',
-    m = 'médio',
-    g = 'grande',
+interface IVehicle {
+    brand: string;
+    showBrand(): void;
 }
-const camisa = { //utilização da enumeração em uma objetos
-    nome: "camisa kkk",
-    size: size.g,
+
+class vehicle implements IVehicle {
+    brand;
+    whell;
+    maxSpeed;
+
+    constructor(brand: string, whell: number, maxSpeed: string) {
+        this.brand = brand;
+        this.whell = whell;
+        this.maxSpeed = maxSpeed;
+    }
+
+    showBrand(): void {
+        console.log(`A marca do vehículo é ${this.brand}.`);
+    }
 }
-console.log(camisa); //a saída da variável "size" de camisa fica com o valo de g em "enum size";
 
-//--------------------------------------------------------------------------------------------
+const fiatUnoDeFirma = new vehicle('Fiat', 16, 'luz');
+console.log(fiatUnoDeFirma);
+fiatUnoDeFirma.showBrand();
 
-// Literal types -> criar uma variável instantânea que acompanha a variável. Apenas pode ser auterada para a variável declarada;
+//herança
 
-let literal: "autenticado" | null; //pode ser usado em conjunto com o "null" para variar entre os dois tipos
-// literal = "outro tipo ou valor qualquer" -> não funciona;
-literal = null;
-literal = "autenticado"; //apenas
+class superCar extends vehicle {
+    engine;
 
-//--------------------------------------------------------------------------------------------
-
-//funçoes
-
-function soma(a: number, b: number): number { //explicite o que a função irá retornar
-    return a + b; //lembre sempre de específicar os timos das variáveis recebias.
+    constructor(brand: string, wheel: number, maxSpeed: string, engine: string) {
+        super(brand, wheel, maxSpeed);
+        this.engine = engine;
+    }
 }
-console.log(soma(5, 5));
 
-function sayHelloTo(name: string): string {
-    return `Hello to ${name}`; //strings com váriáveis pelo meio usam ``
-}
-console.log(sayHelloTo('Marcelo'));
+const Veiron = new superCar('Bugati', 20, '300km/h', 'v8');
+console.log(Veiron);
+Veiron.showBrand();
 
-function logger(msg: string): void { //usando o tipo "void" para especificar o tipo de função que não gera retorno
-    console.log(msg); //apesar de o "console.log" retornar para o terminal ele não retorna nenhum valor ao código, por isso a função é vazia
-}
-logger('não retorno nada');
+
+//------------------------------------------------------------------------------------------
+
